4b07405f8397c6272ab508789462591a7eda6c45
Luke Hoban <luke@pulumi.com>
2018-09-27 06:57:12 -0700

    Simplify example again for now
    We'll want to add more coverage as part of #1, but for now, keep the example simple.

--- example/index.js
+++ example/index.js
@@ -7,36 +7,5 @@ require("@pulumi/iopipe")(pulumi);
 
 // Create a bucket and a function to log new object uploads
 const bucket = new aws.s3.Bucket("my-bucket");
-//serverless.bucket.onPut("onNewObject", bucket, async (ev) => console.log(ev));
-
-let f = new aws.serverless.Function("f", {
-    policies: [aws.iam.AWSLambdaFullAccess],
-    factoryFunc: () => {
-        console.log("booting!");
-        return async (ev) => {
-            console.log("factoryFunc")
-            console.log(ev);
-        }
-    },
-})
-serverless.bucket.onPut("onNewObject2", bucket, f.lambda);
+serverless.bucket.onPut("onNewObject", bucket, async (ev) => console.log(ev));
 exports.bucketName = bucket.bucket;
-
-const hello = new aws.serverless.Function('hello', {
-    func: (event1, context, cb) => {
-        console.log(event1)
-        cb(null, {
-            statusCode: 200,
-            body: "Hello world",
-        });
-    },
-    policies: ['arn:aws:iam::aws:policy/AWSLambdaFullAccess']
-})
-
-const api = new serverless.apigateway.API('api', {
-    routes: [
-        {method: 'GET', path: '/hello', handler: hello.lambda}
-    ]
-})
-
-exports.endpoint = api.url;
